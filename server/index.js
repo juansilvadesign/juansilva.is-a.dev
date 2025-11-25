c0645889eeac07f86a1c32e32cb7a4416ed3a0ed
JuanPabloDesigner <jaypy.uxdesign@gmail.com>
2025-11-25 01:01:21 -0300

    refactor: mail smtp server updated

--- server/index.js
+++ server/index.js
@@ -74,7 +74,9 @@ app.use(cors(corsOptions));
 app.use('/api/send', limiter); // Aplica rate limiting na rota de envio
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
